c9261c2bb279d6f1032a8af6e1c38df02d3a8ddf
wjh2012 <wjh2012@naver.com>
2024-10-06 18:58:39 +0900

    Initial commit

---
+++ src/main/kotlin/com/ggomg/teamsauth/TeamsAuthApplication.kt
@@ -0,0 +1,11 @@
+package com.ggomg.teamsauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamsAuthApplication
+
+fun main(args: Array<String>) {
+    runApplication<TeamsAuthApplication>(*args)
+}
---
+++ src/test/kotlin/com/ggomg/teamsauth/TeamsAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ggomg.teamsauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamsAuthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
